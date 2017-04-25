66c1581d9fc216beebab417a41c51dda9bf8a551
Raptorkiller <raptorkiller@gmx.at>
2017-04-25 02:44:35 +0200

    Basic shoot system, bullet,Health

---
+++ Assets/Bullet.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    public float bulletDamage;
+    public float bulletVelocity;
+    public float maxRange;
+
+   
+    private Vector3 spawnPosition;
+    private Quaternion spawnRotation;
+    private Vector3 currentPosition;
+    private Rigidbody rigidBody;
+   
+
+    // Use this for initialization
+    void Start () {
+        spawnPosition = transform.position;
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentPosition = transform.position;
+        
+        if (Mathf.Abs(spawnPosition.z - currentPosition.z) < maxRange && Mathf.Abs(spawnPosition.x - currentPosition.x) < maxRange && Mathf.Abs(spawnPosition.y - currentPosition.y) < maxRange)
+        {
+            rigidBody.AddForce(transform.forward * bulletVelocity);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        
+       
+        
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Health target = collision.gameObject.GetComponent<Health>();
+
+        if (target && collision.gameObject.tag != "Player")
+        {
+            target.health -= bulletDamage;
+            Destroy(gameObject);
+        }
+    }
+
+    
+
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+    public GameObject bullet;
+    public float RPM;
+    private float roundsPerSecond;
+    private float fireRate;
+    public Vector3 offSet;
+    private Camera eyes;
+    private float timer;
+    
+    // Use this for initialization
+	void Start () {
+        eyes = GetComponentInChildren<Camera>();
+        roundsPerSecond = 60f / RPM;
+        Debug.Log(roundsPerSecond);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer -= Time.deltaTime;
+        if (Input.GetButton("Fire1") && timer <=0)
+        {
+            Instantiate(bullet, eyes.transform.position + offSet, eyes.transform.rotation);
+            timer = roundsPerSecond; ;
+        }
+        else
+        {
+            
+        }
+	}
+}
